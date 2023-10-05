1813d2bfe64528dacb2ece2c033449afd9a3d94c
SteelWagstaff <steel@pressbooks.com>
2023-10-05 11:43:41 -0700

    feat: handle 4 new shortcodes

--- pressbooks-shortcode-handler.php
+++ pressbooks-shortcode-handler.php
@@ -4,7 +4,7 @@
 Plugin Name: Pressbooks Shortcode Handler
 Plugin URI: https://github.com/SteelWagstaff/pressbooks-shortcode-handler
 Description: Plugin for Pressbooks to handle additional shortcodes used by Lumen Learning.
-Version: 1.0.1
+Version: 1.1.0
 Author: Steel Wagstaff
 Author URI: https://steelwagstaff.info
 License: GPL 3.0
@@ -15,6 +15,10 @@ add_shortcode( 'hidden-answer', 'hiddenAnswerShortCodeHandler' );
 add_shortcode( 'glossary-page', 'glossary_page_shortcode' );
 add_shortcode( 'glossary-term', 'glossary_term_shortcode' );
 add_shortcode( 'glossary-definition', 'glossary_definition_shortcode' );
+add_shortcode( 'videopicker', 'videopicker_shortcode' );
+add_shortcode( 'ohm', 'ohm2_shortcode_handler' );
+add_shortcode( 'ohm2_question', 'ohm2_question_shortcode' );
+add_shortcode( 'choosedataset', 'choosedataset_shortcode' );
 
 /**
  * Shortcode handler for [reveal-answer].
@@ -25,7 +29,7 @@ add_shortcode( 'glossary-definition', 'glossary_definition_shortcode' );
  * @return string
  */
 function revealAnswerShortCodeHandler( $atts = [], $content = null ) {
-    return '<details><summary>' . do_shortcode( $content ) . '</summary>';
+	return '<details><summary>' . do_shortcode( $content ) . '</summary>';
 }
 
 /**
@@ -38,7 +42,7 @@ function revealAnswerShortCodeHandler( $atts = [], $content = null ) {
  */
 
 function hiddenAnswerShortCodeHandler( $atts = [], $content = null ) {
-    return do_shortcode( $content ) . '</details>';
+	return do_shortcode( $content ) . '</details>';
 }
 
 /**
@@ -50,7 +54,7 @@ function hiddenAnswerShortCodeHandler( $atts = [], $content = null ) {
  * @return string
  */
 function glossary_page_shortcode( $atts = [], $content = null ) {
-    return '<div class="lumen-glossary"><dl>' . do_shortcode( $content ) . '</dl></div>';
+	return '<div class="lumen-glossary"><dl>' . do_shortcode( $content ) . '</dl></div>';
 }
 
 /**
@@ -62,7 +66,7 @@ function glossary_page_shortcode( $atts = [], $content = null ) {
  * @return string
  */
 function glossary_term_shortcode( $atts = [], $content = null ) {
-    return '<dt>' . do_shortcode( $content ) . '</dt>';
+	return '<dt>' . do_shortcode( $content ) . '</dt>';
 }
 
 /**
@@ -73,5 +77,60 @@ function glossary_term_shortcode( $atts = [], $content = null ) {
  * @return string
  */
 function glossary_definition_shortcode( $atts = [], $content = null ) {
-    return '<dd>' . do_shortcode( $content ) . '</dd>';
+	return '<dd>' . do_shortcode( $content ) . '</dd>';
+}
+
+
+/**
+ * Shortcode that displays a fallback message for excluded interactive video picker elements used by Lumen Learning
+ * @param string $content Shortcode content.
+ *
+ * @return string
+ */
+function videopicker_shortcode( $atts = [], $content = null ) {
+	return '<div class="textbox interactive-content">
+	<span class="interactive-content__icon"></span>
+	<p>An interactive video picker element has been excluded from the printed version of the text. To see the interactive element that was excluded, please visit the courseware online.</p>
+	</div>';
+}
+
+/**
+ * Shortcode that displays the title, label, and brief fallback message for excluded interactive dataset picker elements
+ * @param string $content Shortcode content.
+ *
+ * @return string
+ */
+function choosedataset_shortcode( $atts = [], $content = null ) {
+	$header = '';
+	if ( $atts['title'] ) {
+		$header.= '<h3>' . $atts['title'] . '</h3>';
+	}
+	if ( $atts['label'] ) {
+		$header.= '<h4>' . $atts['label'] . '</h4>';
+	}
+	$options = '';
+	if ($atts['default']) {
+		$options.= '<option value="">'. $atts['default'] .'</option>';
+	}
+	$id = $atts['divid'] ? $atts['divid'] : 'tnh-choose-dataset';
+
+	return '<div id="' . $id .'" class="chooseDataset">
+            ' . $header . '
+            <div class="textbox interactive-content">
+	<span class="interactive-content__icon"></span>
+	<p>An interactive dataset picker element has been excluded from the printed version of the text. To see the interactive element that was excluded, please visit the courseware online.</p>
+	</div></div>';
+}
+
+/**
+ * Shortcode that displays a brief fallback message for excluded OHM & OHM 2 questions used by Lumen Learning
+ * @param string $content Shortcode content.
+ *
+ * @return string
+ */
+function ohm2_question_shortcode( $atts = [], $content = null ) {
+	return '<div class="textbox interactive-content">
+	<span class="interactive-content__icon"></span>
+	<p>An interactive online homework element has been excluded from the printed version of the text. To see the interactive element that was excluded, please visit the courseware online.</p>
+	</div>';
 }
